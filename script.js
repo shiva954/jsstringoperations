300e0cc384e8c0aed64106205691f66b42c386df
shiva954 <orsushiva138@gmail.com>
2024-11-04 21:35:15 +0530

    meassage

---
+++ script.js
@@ -0,0 +1,8 @@
+let firstname = "devaram";
+let lastname = "roshan";
+console.log(`the concat of ${firstname} and ${lastname} is ${firstname + lastname}`);
+
+let string = "hello everyone how are you?";
+console.log(`the split of the string "${string}" is ${string.split(" ")}`);
+console.log(`the slice of the string "${string}" from index 0 to 6 is ${string.slice(0, 7)}`);
+console.log(`the string "${string}" constains the word "everyone" ${string.includes("everyone")}`);
